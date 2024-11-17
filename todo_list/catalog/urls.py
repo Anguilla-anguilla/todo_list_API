d229943333b38263c436116e8349eb8eeafdbcde
Nika <srkveronica@gmail.com>
2024-11-17 16:55:38 +0300

    Add TodoDetailAPIView class

--- todo_list/catalog/urls.py
+++ todo_list/catalog/urls.py
@@ -6,4 +6,5 @@ app_name = 'catalog'
 
 urlpatterns = [
     path('api', views.TodoAPIView.as_view()),
+    path('api/<int:todo_id>', views.TodoDetailAPIView.as_view()),
 ]
--- todo_list/catalog/views.py
+++ todo_list/catalog/views.py
@@ -27,3 +27,45 @@ class TodoAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TodoDetailAPIView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_objects(self, todo_id, user_id):
+        try:
+            return Todo.objects.get(id=todo_id, user=user_id)
+        except Todo.DoesNotExist:
+            return None
+        
+    def get(self, request, todo_id):
+        todo_instance = self.get_objects(todo_id, request.user.id)
+        if not todo_instance:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        serializer = TodoSerializer(todo_instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    # update
+    def put(self, request, todo_id):
+        todo_instance = self.get_objects(todo_id, request.user.id)
+        if not todo_instance:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        data = {
+            'title': request.data.get('title'),
+            'description': request.data.get('description'),
+            'user': request.user.id
+        }
+        serializer = TodoSerializer(isinstance=todo_instance, 
+                                    data-data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, todo_id):
+        todo_instance = self.get_objects(todo_id, request.user.id)
+        if not todo_instance:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        todo_instance.delete()
+        return Response(status=status.HTTP_200_OK)
+    
